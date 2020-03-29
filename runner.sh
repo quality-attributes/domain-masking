ab6ce50e36d3ce6c808c4a52d5fe568acea9fe36
Manolo Perez <manolope338@gmail.com>
2020-03-29 10:39:41 -0600

    Update results executions

--- runner.sh
+++ runner.sh
@@ -4,11 +4,11 @@ python3 -m venv venv
 
 pip install -r requirements.txt
 
-ludwig experiment --comet --data_csv clean-train.csv --model_definition_file model_definition.yaml
-ludwig experiment --comet --data_csv raw-test.csv --model_definition_file model_definition.yaml
-
 ludwig experiment --model_name clean --experiment_name domain-masking --data_train_csv train.csv --data_test_csv test.csv --model_definition_file model_definition.yaml --comet
 ludwig experiment --model_name raw --experiment_name domain-masking --data_train_csv raw_train.csv --data_test_csv raw_test.csv --model_definition_file model_definition.yaml --comet
 
-ludwig visualize --visualization compare_classifiers_multiclass_multimetric  --test_statistics results/clean/test_statistics.json --ground_truth_metadata results/clean/model/train_set_metadata.json
+ludwig visualize --visualization confusion_matrix --test_statistics results/domain-masking_clean/test_statistics.json --ground_truth_metadata results/domain-masking_clean/model/train_set_metadata.json --normalize
+ludwig visualize --visualization confusion_matrix --test_statistics results/domain-masking_raw/test_statistics.json --ground_truth_metadata results/domain-masking_raw/model/train_set_metadata.json --normalize
+
+ludwig visualize --visualization compare_classifiers_multiclass_multimetric  --test_statistics results/domain-masking_clean/test_statistics.json --ground_truth_metadata results/domain-masking_clean/model/train_set_metadata.json --comet
 ludwig visualize --visualization compare_classifiers_multiclass_multimetric  --test_statistics results/raw/test_statistics.json --ground_truth_metadata results/raw/model/train_set_metadata.json
